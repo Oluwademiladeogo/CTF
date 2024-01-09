c0d00498ffaaa7c4814fd77f644e61b894b5df28
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-01-09 12:59:07 +0100

    Feat: include login and signup route

--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
-import express from "express";
+import express from 'express';
+import signupRouter from './users/signup';
+import loginRouter from './users/login';
+
 const app = express();
 app.use(express.json());
-//** Production */
+app.use('/signup', signupRouter);
+app.use('/signup', loginRouter);
 
 export default app;
