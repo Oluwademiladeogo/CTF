2fb093627334a90d17fdb373305d6b708218e997
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-01-09 20:51:17 +0100

    Chore: add logout route

--- app.ts
+++ app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import signupRouter from './users/signup';
 import loginRouter from './users/login';
-
+import logoutRouter from './users/logout';
 const app = express();
 app.use(express.json());
 app.use('/signup', signupRouter);
 app.use('/login', loginRouter);
+app.use('logout', logoutRouter);
 
 export default app;
