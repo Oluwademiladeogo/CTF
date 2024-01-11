4bb2da7c0d77a2a6d4cc24d9ecf94a9ed7da9873
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-01-11 15:57:21 +0100

    Feat: add signup route

--- users/signup.ts
+++ users/signup.ts
@@ -1,12 +1,11 @@
 import express, { Request, Response } from 'express';
-import { User } from '../models/users.model';
 import { signupUser } from './user.controller';
 const router = express.Router();
 router.get('/', (_req: Request, res: Response) => {
-    res.status(404).send('Page not found');
+    res.status(404).json({ message: 'Page not found' });
 });
 router.post('/', async (req: Request, res: Response) => {
-    const data = await signupUser(req.body);
-    res.status(data.status).json({ message: data.message });
+    const { status, message } = await signupUser(req.body);
+    res.status(status).json({ message: message });
 });
 export default router;
