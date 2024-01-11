04bdd21499eb14473f2a5274eb0506d229fdb472
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-01-11 15:53:22 +0100

    Feat: add route for sending otp to a user

---
+++ users/sendOtp.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response } from 'express';
+import { sendOtp } from '../utils/otp';
+const router = express.Router();
+router.post('/', async (req: Request, res: Response) => {
+    const { email } = req.body;
+    const { status, message } = await sendOtp(email);
+    res.status(status).json({ message: message });
+});
--- users/signup.ts
+++ users/signup.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from 'express';
-import { User } from '../models/users';
+import { User } from '../models/users.model';
 import { signupUser } from './user.controller';
 const router = express.Router();
 router.get('/', (_req: Request, res: Response) => {
     res.status(404).send('Page not found');
 });
 router.post('/', async (req: Request, res: Response) => {
-    const data = await signupUser(req.body)
-    res.status(data.status).json({message: data.message})
+    const data = await signupUser(req.body);
+    res.status(data.status).json({ message: data.message });
 });
 export default router;
