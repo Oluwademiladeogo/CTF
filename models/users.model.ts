e5b5a2da01ded7a517e07ccca8cfdafec18c6faa
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-01-11 12:11:57 +0100

    db: add role to users model

---
+++ models/users.model.ts
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose';
+export interface UserDoc extends Document {
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone_num: string;
+    birthday: Date;
+    password: string;
+    salt: string;
+    role: string;
+}
+export const User = mongoose.model<UserDoc>(
+    'Users',
+    new mongoose.Schema<UserDoc>(
+        {
+            first_name: {
+                type: String,
+                minlength: 2,
+                maxlength: 255,
+                required: true,
+            },
+            last_name: {
+                type: String,
+                minlength: 2,
+                maxlength: 255,
+                required: true,
+            },
+            email: {
+                type: String,
+                minlength: 2,
+                maxlength: 255,
+                required: true,
+                unique: true,
+            },
+            phone_num: {
+                type: String,
+                minlength: 10,
+                maxlength: 20,
+                required: true,
+                unique: true,
+            },
+            birthday: {
+                type: Date,
+                required: true,
+            },
+            password: {
+                type: String,
+                required: true,
+                minlength: 8,
+            },
+            salt: {
+                type: String,
+                required: true,
+            },
+            role: {
+                type: String,
+                enum: ['User', 'Admin'],
+            },
+        },
+        { timestamps: true },
+    ),
+);
