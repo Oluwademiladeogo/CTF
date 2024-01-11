ca789f1e157895dc15389ffec1bcd805bf2c0d88
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-01-11 16:06:13 +0100

    Chore: code refactor

--- users/verifyOtp.ts
+++ users/verifyOtp.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from "express"
-import { verifyUserOtp } from "../utils/otp"
-const router = express.Router()
-router.post("/", async(req:Request, res:Response)=>{
-    const {email, pass} = req.body
-    const {status, message} = await verifyUserOtp(email, pass)
-    res.status(status).json({message: message})
-})
+import express, { Request, Response } from 'express';
+import { verifyUserOtp } from '../utils/otp';
+const router = express.Router();
+router.post('/', async (req: Request, res: Response) => {
+    const { email, pass } = req.body;
+    const { status, message } = await verifyUserOtp(email, pass);
+    res.status(status).json({ message: message });
+});
