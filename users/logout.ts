43332fa60e85b5659ed9500fb01d3c4c553d3a2d
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-01-11 15:54:51 +0100

    Feat: add status to logout response

--- users/logout.ts
+++ users/logout.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 const router = express.Router();
 router.get('/', (_req: Request, res: Response) => {
-    res.clearCookie('authToken').send('logout successful');
+    res.status(200).clearCookie('authToken').send('logout successful');
 });
 export default router;
